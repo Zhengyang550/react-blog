e7d3a913553d0f8dc2592c818ffec6c336067b1d
Zhengyang550 <18151521911@163.com>
2020-09-16 11:30:12 +0800

    更改后端接口

--- src/redux/index.js
+++ src/redux/index.js
@@ -5,12 +5,18 @@
  * 不懂原理的可以参考：https://github.com/brickspert/blog/issues/22#middleware
  */
 import thunk from 'redux-thunk';
-import {compose, createStore, applyMiddleware} from 'redux';
+//applyMiddleware用来合并多个中间件，逗号隔开
+import {createStore, applyMiddleware} from 'redux';
 import rootReducers from './root_reducers';
+//redux的可视化工具，谷歌的应用商城工具
 import {composeWithDevTools} from 'redux-devtools-extension';
+// 调用日志打印方法 collapsed是让action折叠，看着舒服点
+import { createLogger } from 'redux-logger';
 
-const storeEnhancers = process.env.NODE_ENV === 'production' ? compose(applyMiddleware(thunk)) :
-    compose()(composeWithDevTools(applyMiddleware(thunk)));
+
+//这里判断项目环境，正式的话打印的，和可视化的中间件可以去掉
+const storeEnhancers = process.env.NODE_ENV === 'production' ? applyMiddleware(thunk) :
+   composeWithDevTools(applyMiddleware(thunk,createLogger()));
 
 /**
  * 创建store
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(createProxyMiddleware('/blog/api',
         {
-            target: 'http://127.0.0.1:30001',
+            target: 'http://140.143.25.72:30001/',
             changeOrigin: true
         }
     ));
